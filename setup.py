eea42da9796b8b8ce6a20579f314c9cc410985d6
John Thornton <jthornton@gnipsel.com>
2019-05-12 16:28:20 -0500

    try to get setup to copy mesaflash

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
+import os
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+ # list of (destination, source_file) tuples
+DATA_FILES = [('~/.local/bin', ['mesaflash64', 'mesaflash32',])]
+data_files = [(os.path.expanduser(dest), src_list) for dest, src_list in DATA_FILES]
 
 setup(
     name="7i76e",
@@ -15,6 +20,7 @@ setup(
     download_url="https://github.com/jethornton/7i76e/tarball/master",
     python_requires='>=3',
     packages=find_packages(),
+    data_files=data_files,
     include_package_data=True,
     entry_points={
         'gui_scripts': ['7i76e=m7i76e.7i76e:main',],
